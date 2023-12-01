fe1aedaeea07f10d44beabc98c4929df7dbb0c51
Gelson Carlos <gelsoncjunior@hotmail.com>
2023-12-01 11:53:16 -0300

    Assets

--- unilever-assets-proxy.js
+++ unilever-assets-proxy.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const request = require('request');
+const axios = require('axios');
 const app = express();
-const PORT = 5556; // Porta em que o servidor Node.js irá rodar
+const PORT = 5556;
 
-app.get('*', (req, res) => {
+app.get('*', async (req, res) => {
     const bucketUrl = 'https://unlstoragecdn.azureedge.net/'+req.url+'?sv=2022-11-02&ss=bfqt&srt=sco&sp=r&se=2033-05-11T01:48:57Z&st=2023-05-10T17:48:57Z&spr=https&sig=3QvWLORE0Wnr%2FnJHpD%2BBCtwnFMbo1h3pbzVbJZiLiKs%3D';
     console.log(`Requisitando ${bucketUrl}`);
     try {
-        request(bucketUrl).pipe(res);
+        const response = await axios.get(bucketUrl, { responseType: 'stream' });
+        response.data.pipe(res);
     } catch (error) {
-        console.log(error);
+        console.error(error);
+        res.status(500).send('Erro interno do servidor');
     }
 });
 
