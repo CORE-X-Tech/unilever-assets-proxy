17bfb9537b6ca392f541448b9c72a8e71eaf1673
Gelson Carlos <gelsoncjunior@hotmail.com>
2023-12-01 11:06:01 -0300

    Assets

--- unilever-assets-proxy.js
+++ unilever-assets-proxy.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const request = require('request');
 const app = express();
-const PORT = 5555; // Porta em que o servidor Node.js irá rodar
+const PORT = 5556; // Porta em que o servidor Node.js irá rodar
 
 app.get('*', (req, res) => {
     const bucketUrl = 'https://unlstoragecdn.azureedge.net/'+req.url+'?sv=2022-11-02&ss=bfqt&srt=sco&sp=r&se=2033-05-11T01:48:57Z&st=2023-05-10T17:48:57Z&spr=https&sig=3QvWLORE0Wnr%2FnJHpD%2BBCtwnFMbo1h3pbzVbJZiLiKs%3D';
